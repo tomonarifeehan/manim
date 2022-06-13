a8ca00def2401994366b58720f80cfc15baa3892
Tomonari Feehan <20110627+tomonarifeehan@users.noreply.github.com>
2022-06-13 04:51:37 -0400

    Smaller font for content text.

--- chain_rule/part1.py
+++ chain_rule/part1.py
@@ -28,24 +28,24 @@ class Title(Scene):
 class Content(Scene):  
     def construct(self):
 
-        textbox = create_textbox(height = 5, width = 8, color = TEAL_B, string = "")
+        textbox = create_textbox(height = 5, width = 10, color = TEAL_B, string = "")
         self.add(textbox)
 
-        textbox.shift(1 * DOWN + 2.5 * LEFT)
+        textbox.shift(1 * DOWN + 1.5 * LEFT)
 
 class ChainRule(Scene):
     def construct(self):
         
         # h(x) = f[g(x)]
-        equationOne = TexText(latexOne)
+        equationOne = TexText(latexOne, font_size = 40)
         equationOne.shift(0.5 * UP + 4 * LEFT)
         
         # h'(x) = f'[g(x)] * g'(x)
-        equationTwo_partOne = TexText(latexTwo_partOne)
+        equationTwo_partOne = TexText(latexTwo_partOne, font_size = 40)
         equationTwo_partOne.shift(1 * DOWN + 3.7 * LEFT)
 
-        equationTwo_partTwo= TexText(latexTwo_partTwo)
-        equationTwo_partTwo.shift(1 * DOWN + 0.6 * LEFT)
+        equationTwo_partTwo = TexText(latexTwo_partTwo, font_size = 40)
+        equationTwo_partTwo.shift(1 * DOWN + 1.1 * LEFT)
         
         self.wait(1)
 
